4604e47b77326a60f38e2be894e7d4d01cc3479d
Jared Griggs <griggsjared@gmail.com>
2024-12-27 17:45:56 -0700

    update various nvim config

--- nvim/.config/nvim/lua/plugins/color-theme.lua
+++ nvim/.config/nvim/lua/plugins/color-theme.lua
@@ -3,23 +3,28 @@ return {
     "loctvl842/monokai-pro.nvim",
     lazy = false,
     config = function()
-      local pallete = require("monokai-pro.override")
+      local palette = require("monokai-pro.override")
+
+      local transparent_background = true;
+      if vim.g.neovide then
+        transparent_background = false
+      end
 
       require("monokai-pro").setup({
         devicons = true,
-        transparent_background = true,
+        transparent_background = transparent_background,
         terminal_colors = true,
         filter = "pro",
         overridePalette = function()
-          return pallete
+          return palette
         end,
       })
 
       vim.cmd([[colorscheme monokai-pro]])
 
-      vim.api.nvim_set_hl(0, "FloatBorder", { bg = pallete.dark2, fg = pallete.dimmed2 })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = pallete.dark2, fg = pallete.text })
-      vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = pallete.cyan })
+      vim.api.nvim_set_hl(0, "FloatBorder", { bg = palette.dark2, fg = palette.dimmed2 })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = palette.dark2, fg = palette.text })
+      vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = palette.cyan })
     end,
   }
 }

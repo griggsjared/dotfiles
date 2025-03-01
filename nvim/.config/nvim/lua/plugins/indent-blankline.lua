c7525aae5425314546911ccde07a366815813ae0
Jared Griggs <griggsjared@gmail.com>
2025-03-01 15:49:01 -0700

    feat(nvim): add config for onedark with some darker bg mods, adjust color theme config in general

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -3,25 +3,21 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     config = function()
-      local highlight = {
-        "IndentRed",
-        "IndentYellow",
-        "IndentOrange",
-        "IndentGreen",
-        "IndentMagenta",
-        "IndentCyan",
-      }
-
-      local inactive_highlight = {
-        "IndentInactive",
-      }
-
       local hooks = require("ibl.hooks")
 
       require("ibl").setup({
-        scope = { highlight = highlight },
-        indent = { char = "┊", highlight = inactive_highlight },
-      })
+        scope = {
+          highlight = {
+            "IndentActive",
+          }
+        },
+        indent = {
+          char = "┊",
+          highlight = {
+            "IndentInactive"
+          }
+        },
+      });
 
       hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
     end,
---
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,106 @@
+return {
+  {
+    "loctvl842/monokai-pro.nvim",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      local default_palette = require('monokai-pro.colorscheme.palette.pro')
+      local palette = vim.tbl_extend("force", default_palette, {
+        -- overrides
+        background = "#1a1a1a",
+        dark1 = "#090909",
+        dark2 = "#0a0a0a",
+        -- aliases
+        red = default_palette.accent1,
+        orange = default_palette.accent2,
+        yellow = default_palette.accent3,
+        green = default_palette.accent4,
+        cyan = default_palette.accent5,
+        magenta = default_palette.accent6,
+      })
+
+      local highlights = {
+        FloatBorder = { bg = palette.dark2, fg = palette.dimmed2 },
+        NormalFloat = { bg = palette.dark2, fg = palette.text },
+        CmpItemKindCopilot = { fg = palette.cyan },
+        Visual = { bg = palette.dimmed4 },
+        -- Indent Blankline
+        IndentActive = { fg = palette.red },
+        IndentInactive = { fg = palette.dimmed4 },
+        -- Render Markdown
+        RenderMarkdownHeader = { bg = palette.dark1, fg = palette.green },
+        RenderMarkdownCode = { bg = palette.dark1 },
+        RenderMarkdownCodeInline = { bg = palette.dark1 },
+      }
+
+      local transparent_background = true;
+      if vim.g.neovide then
+        transparent_background = false
+      end
+
+      require("monokai-pro").setup({
+        devicons = true,
+        transparent_background = transparent_background,
+        terminal_colors = true,
+        filter = "pro",
+        styles = {
+          comment = { italic = true },
+          keyword = { italic = true },
+          type = { italic = true },
+          storageclass = { italic = true },
+          structure = { italic = true },
+          parameter = { italic = true },
+          annotation = { italic = true },
+          tag_attribute = { italic = true },
+        },
+        overridePalette = function()
+          return palette
+        end,
+        override = function()
+          return highlights
+        end
+      })
+      vim.cmd([[colorscheme monokai-pro]])
+    end,
+  },
+  {
+    "olimorris/onedarkpro.nvim",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      local background = "#1a1a1a";
+      require("onedarkpro").setup({
+        colors = {
+          bg = background,
+        },
+        options = {
+          transparency = false
+        },
+        highlights = {
+          Normal = { bg = "${none}" },
+          IndentActive = { fg = "${red}" },
+          IndentInactive = { fg = "${gray}" },
+        },
+        styles = {
+          types = "italic",
+          methods = "NONE",
+          numbers = "NONE",
+          strings = "NONE",
+          comments = "italic",
+          keywords = "bold",
+          constants = "NONE",
+          functions = "italic",
+          operators = "NONE",
+          variables = "NONE",
+          parameters = "NONE",
+          conditionals = "NONE",
+          virtual_text = "NONE",
+        },
+        filetypes = {
+          php = false
+        }
+      })
+      -- vim.cmd([[colorscheme onedark]])
+    end,
+  }
+}

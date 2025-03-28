8adad96a2806b9dbac950b5ed615b75517539bd1
Jared Griggs <griggsjared@gmail.com>
2025-03-27 20:50:18 -0600

    chore(nvim): clean up blade from treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -19,23 +19,7 @@ return {
           "lua",
           "go",
           "templ",
-        },
-      })
-
-      -- Manually installing the Blade parser:
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.blade = {
-        install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "blade",
-      }
-
-      vim.filetype.add({
-        pattern = {
-          [".*%.blade%.php"] = "blade",
+          "blade"
         },
       })
     end,

9ec5f1feb15c2cc710c1bdf54081bfb5a33389f6
Jared Griggs <griggsjared@gmail.com>
2025-04-12 14:01:25 -0600

    feat(nvim): update inactive code compantion lua line

--- nvim/.config/nvim/lua/plugins/code-companion/lualine-adapter.lua
+++ nvim/.config/nvim/lua/plugins/code-companion/lualine-adapter.lua
@@ -1,12 +1,12 @@
 local M = require("lualine.component"):extend()
 
 function M:update_status()
-  local chat = require("codecompanion").buf_get_chat(vim.api.nvim_get_current_buf())
-  if not chat then
-    return nil
-  end
+	local chat = require("codecompanion").buf_get_chat(vim.api.nvim_get_current_buf())
+	if not chat then
+		return nil
+	end
 
-  return " " .. chat.adapter.formatted_name
+	return " " .. chat.adapter.formatted_name
 end
 
 return M

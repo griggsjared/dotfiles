134e0ed37308dec9483e7f804fdf5bc354ab1d71
Jared Griggs <griggsjared@gmail.com>
2025-04-12 13:42:41 -0600

    feat(nvim): add lualine code companion adapter and model components, remove fidget spinner verion

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,25 +1,68 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("lualine").setup({
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = {
-          {
-            "filename",
-            path = 1,
-          },
-        },
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-      options = {
-        section_separators = "",
-        component_separators = "",
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+    local codecompanion_model = require("plugins.code-companion.lualine-model")
+    local codecompanion_adapter = require("plugins.code-companion.lualine-adapter")
+		require("lualine").setup({
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+				lualine_x = {
+					{
+						"lsp_status",
+						icon = "",
+					},
+					"encoding",
+					"fileformat",
+					"filetype",
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			options = {
+				section_separators = "",
+				component_separators = "",
+			},
+			extensions = {
+				{
+					filetypes = { "codecompanion" },
+					sections = {
+						lualine_a = { "mode" },
+						lualine_b = {
+							codecompanion_adapter,
+						},
+						lualine_c = {
+							codecompanion_model,
+						},
+						lualine_x = {},
+						lualine_y = {
+							"progress",
+						},
+						lualine_z = {
+							"location",
+						},
+					},
+					inactive_sections = {
+						lualine_a = {},
+						lualine_b = {
+							codecompanion_adapter,
+						},
+						lualine_c = {},
+						lualine_x = {},
+						lualine_y = {
+							"progress",
+						},
+						lualine_z = {},
+					},
+				},
+			},
+		})
+	end,
 }

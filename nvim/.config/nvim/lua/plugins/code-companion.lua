cdee0036f2b68518ea944b47c916de9df40bec52
Jared Griggs <griggsjared@gmail.com>
2025-04-12 13:42:41 -0600

    feat(nvim): add lualine code companion adapter and model components, remove fidget spinner verion

--- nvim/.config/nvim/lua/plugins/code-companion.lua
+++ nvim/.config/nvim/lua/plugins/code-companion.lua
@@ -64,8 +64,6 @@ return {
         }
       })
 
-      require("plugins.code-companion.fidget-spinner"):init()
-
       vim.keymap.set({ "n", "v" }, "<leader>cq", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
       vim.keymap.set({ "n", "v" }, "<leader>cc", "<cmd>CodeCompanionChat Toggle<cr>",
         { noremap = true, silent = true })
---
+++ nvim/.config/nvim/lua/plugins/code-companion/lualine-adapter.lua
@@ -0,0 +1,12 @@
+local M = require("lualine.component"):extend()
+
+function M:update_status()
+  local chat = require("codecompanion").buf_get_chat(vim.api.nvim_get_current_buf())
+  if not chat then
+    return nil
+  end
+
+  return " " .. chat.adapter.formatted_name
+end
+
+return M

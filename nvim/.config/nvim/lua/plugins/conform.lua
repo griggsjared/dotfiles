5d43e61289ad7be7ed10178d21751a28dca31fa0
Jared Griggs <griggsjared@gmail.com>
2025-06-07 17:55:22 -0600

    feat(nvim): swap null-ls for conform for formatting

---
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,42 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					vue = { "prettier" },
+					blade = { "blade-formatter" },
+					php = { "pint" },
+					go = { "goimports", "gofmt" },
+					c = { "clang-format" },
+					cpp = { "clang-format" },
+				},
+				formatters = {
+					pint = {
+						require_cwd = true,
+					},
+					prettier = {
+						cwd = require("conform.util").root_file({
+							".prettierrc",
+							".prettierrc.json",
+							".prettierrc.yaml",
+							".prettierrc.yml",
+							".prettierrc.js",
+							",prettierrc.cjs",
+							"prettier.config.js",
+						}),
+						require_cwd = true,
+					},
+				},
+			})
+
+			vim.keymap.set("n", "<leader>gf", function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end, {})
+		end,
+	},
+}
--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -1,90 +1,37 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      on_attach = function() end,
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        require("none-ls.diagnostics.eslint").with({
-          prefer_local = "node_modules/.bin",
-          condition = function(utils)
-            return utils.root_has_file({
-              ".eslintrc.js",
-              ".eslintrc.json",
-              ".eslintrc",
-              ".eslintrc.yml",
-              ".eslintrc.yaml",
-              ".eslint.config.mjs",
-              ".eslint.config.cjs",
-              ".eslint.config.js",
-            })
-          end,
-        }),
-        null_ls.builtins.formatting.prettier.with({
-          prefer_local = "node_modules/.bin",
-          condition = function(utils)
-            return utils.root_has_file({
-              ".prettierrc",
-              ".prettierrc.json",
-              ".prettierrc.yaml",
-              ".prettierrc.yml",
-              ".prettierrc.js",
-              ".prettierrc.cjs",
-              "prettier.config.js",
-            })
-          end,
-          filetypes = {
-            "javascript",
-            "javascriptreact",
-            "typescript",
-            "typescriptreact",
-            "json",
-            "yaml",
-            "markdown",
-            "css",
-            "scss",
-            "html",
-            "vue",
-            "svelte",
-            "graphql",
-            "lua",
-            "astro"
-          },
-        }),
-        null_ls.builtins.formatting.goimports.with({
-          filetypes = { "go" },
-        }),
-        null_ls.builtins.formatting.gofmt.with({
-          filetypes = { "go" },
-        }),
-        null_ls.builtins.formatting.pint.with({
-          prefer_local = "vendor/bin",
-          filetypes = { "php" },
-          condition = function(utils)
-            return utils.root_has_file({ "vendor/bin/pint", "pint.json" })
-          end,
-        }),
-        null_ls.builtins.diagnostics.phpstan.with({
-          condition = function(utils)
-            return utils.root_has_file({
-              "phpstan.neon",
-              "phpstan.neon.dist",
-            })
-          end,
-        }),
-        null_ls.builtins.formatting.blade_formatter.with({
-          filetypes = { "blade" },
-        }),
-        null_ls.builtins.formatting.clang_format.with({
-          filetypes = { "c", "cpp", "objc", "objcpp" },
-        }),
-      },
-    })
-
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			on_attach = function() end,
+			sources = {
+				require("none-ls.diagnostics.eslint").with({
+					prefer_local = "node_modules/.bin",
+					condition = function(utils)
+						return utils.root_has_file({
+							".eslintrc.js",
+							".eslintrc.json",
+							".eslintrc",
+							".eslintrc.yml",
+							".eslintrc.yaml",
+							".eslint.config.mjs",
+							".eslint.config.cjs",
+							".eslint.config.js",
+						})
+					end,
+				}),
+				null_ls.builtins.diagnostics.phpstan.with({
+					condition = function(utils)
+						return utils.root_has_file({
+							"phpstan.neon",
+							"phpstan.neon.dist",
+						})
+					end,
+				}),
+			},
+		})
+	end,
 }

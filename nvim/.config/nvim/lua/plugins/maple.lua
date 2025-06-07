956e7eb015c9126b358d36b935b5be3a71914c85
Jared Griggs <griggsjared@gmail.com>
2025-06-07 17:59:15 -0600

    fix(nvim): add maple notes

---
+++ nvim/.config/nvim/lua/plugins/maple.lua
@@ -0,0 +1,12 @@
+return {
+	{
+		"forest-nvim/maple.nvim",
+		opts = {
+			keymaps = {
+				toggle = "<leader>no",
+				close = "q", -- Key to close the window
+				switch_mode = "m"
+			},
+		},
+	},
+}

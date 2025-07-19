70f8f674e3b2e2355d5d382debcda9b6bc207e63
Jared Griggs <griggsjared@gmail.com>
2025-07-19 15:35:04 -0600

    feat(nvim): add toggle on and off for copilot

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -15,8 +15,17 @@ return {
 			vim.keymap.set("i", "<S-Tab>", function()
 				require("copilot.suggestion").accept()
 			end, { silent = true })
+
+      local copilot_inline_enabled = false;
 			vim.keymap.set("n", "<leader>cs", function()
-				require("copilot.suggestion").toggle_auto_trigger()
+        require("copilot.suggestion").toggle_auto_trigger()
+        if copilot_inline_enabled then
+          copilot_inline_enabled = false
+          print("Copilot inline suggestion disabled")
+        else
+          copilot_inline_enabled = true
+          print("Copilot inline suggestion enabled")
+        end
 			end, { silent = true })
 
 			local copilot_enabled = true

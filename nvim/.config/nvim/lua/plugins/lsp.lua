e4984b1894a3b8393eabdf480f3027b1cbbe1704
Jared Griggs <griggsjared@gmail.com>
2025-07-19 15:41:19 -0600

    feat(nvim): add vtsls vue_ls and laravel_ls lsp config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -17,7 +17,7 @@ return {
 					"tailwindcss",
 					"intelephense",
 					"lua_ls",
-					"ts_ls",
+					"vtsls",
 					"vue_ls",
 					"gopls",
 					"templ",
@@ -25,6 +25,7 @@ return {
 					"rust_analyzer",
 					"zls",
 					"clangd",
+					"golangci_lint_ls",
 				},
 			})
 		end,
@@ -175,17 +176,54 @@ return {
 				},
 			})
 
-			vim.lsp.config("vue_ls", {
-				-- capabilities = capabilities,
-				filetypes = { "vue" },
-				init_options = {
-					vue = {
-						hybridMode = false,
-					},
-					typescript = {
-						tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+			vim.lsp.config("vtsls", {
+				settings = {
+					vtsls = {
+						tsserver = {
+							globalPlugins = {
+								{
+									name = "@vue/typescript-plugin",
+									location = vim.fn.stdpath("data")
+										.. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+									languages = { "vue" },
+									configNamespace = "typescript",
+								},
+							},
+						},
 					},
 				},
+				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+			})
+
+			vim.lsp.config("vue_ls", {
+				on_init = function(client)
+					client.handlers["tsserver/request"] = function(_, result, context)
+						local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
+						if #clients == 0 then
+							vim.notify(
+								"Could not found `vtsls` lsp client, vue_lsp would not work without it.",
+								vim.log.levels.ERROR
+							)
+							return
+						end
+						local ts_client = clients[1]
+
+						local param = table.unpack(result)
+						local id, command, payload = table.unpack(param)
+						ts_client:exec_cmd({
+							title = "tsserver request",
+							command = "typescript.tsserverRequest",
+							arguments = {
+								command,
+								payload,
+							},
+						}, { bufnr = context.bufnr }, function(err, r)
+							local response_data = { { id, r.body } }
+							---@diagnostic disable-next-line: param-type-mismatch
+							client:notify("tsserver/response", response_data)
+						end)
+					end
+				end,
 				settings = {
 					typescript = {
 						inlayHints = {
@@ -218,9 +256,22 @@ return {
 						analyses = {
 							unusedparams = true,
 						},
+						hints = {
+							parameterNames = true,
+						},
 					},
 				},
 			})
+
+			vim.lsp.config("golangci_lint_ls", {
+				root_markers = { ".golangci.yaml", ".golangci.yml" },
+			})
+
+			-- laravel_ls is not part of mason yet, so manually enabled
+      vim.lsp.config("laravel_ls", {
+        root_markers = { "artisan" },
+      })
+			vim.lsp.enable("laravel_ls")
 
 			vim.diagnostic.config({
 				virtual_text = true,
@@ -254,6 +305,10 @@ return {
 						return true
 					end,
 				})
+			end)
+
+			vim.keymap.set("n", "<leader>ih", function()
+				vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 			end)
 		end,
 	},

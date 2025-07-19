374b8049009e29896f335bf9dac9349e49e445f3
Jared Griggs <griggsjared@gmail.com>
2025-07-19 15:36:57 -0600

    feat(nvim): add lualine spinner in place of in buffer spinner for code companion

--- nvim/.config/nvim/lua/plugins/code-companion.lua
+++ nvim/.config/nvim/lua/plugins/code-companion.lua
@@ -1,85 +1,117 @@
 return {
-  {
-    "olimorris/codecompanion.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-      "j-hui/fidget.nvim"
-    },
-    config = function()
-      local spinner = require("plugins.code-companion.spinner")
-      spinner:init()
-
-      require("codecompanion").setup({
-        strategies = {
-          chat = {
-            adapter = "copilot",
-            roles = {
-              llm = function(adapter)
-                return "CodeCompanion (" .. adapter.formatted_name .. ")"
-              end,
-              user = "Jared",
-            },
-            send = {
-              callback = function(chat)
-                vim.cmd("stopinsert")
-                chat:add_buf_message({ role = "llm", content = "" })
-                chat:submit()
-              end,
-              index = 1,
-              description = "Send",
-            },
-          },
-          inline = {
-            adapter = "copilot"
-          },
-        },
-        display = {
-          chat = {
-            window = {
-              position = "right",
-              width = 0.50
-            },
-          },
-        },
-        adapters = {
-          copilot = function()
-            return require("codecompanion.adapters").extend("copilot", {
-              schema = {
-                model = {
-                  default = "claude-sonnet-4",
-                }
-              }
-            })
-          end,
-          openai = function()
-            return require("codecompanion.adapters").extend("openai", {
+	{
+		"olimorris/codecompanion.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"j-hui/fidget.nvim",
+			"ravitemer/codecompanion-history.nvim",
+		},
+		config = function()
+			require("codecompanion").setup({
+				strategies = {
+					chat = {
+						adapter = "copilot",
+						roles = {
+							llm = function(adapter)
+								return "CodeCompanion (" .. adapter.formatted_name .. ")"
+							end,
+							user = "Jared",
+						},
+						send = {
+							callback = function(chat)
+								vim.cmd("stopinsert")
+								chat:add_buf_message({ role = "llm", content = "" })
+								chat:submit()
+							end,
+							index = 1,
+							description = "Send",
+						},
+					},
+					inline = {
+						adapter = "copilot",
+					},
+				},
+				display = {
+					chat = {
+						window = {
+							position = "right",
+							width = 0.50,
+						},
+					},
+				},
+				adapters = {
+					copilot = function()
+						return require("codecompanion.adapters").extend("copilot", {
+							schema = {
+								model = {
+									default = "claude-sonnet-4",
+								},
+							},
+						})
+					end,
+					openai = function()
+						return require("codecompanion.adapters").extend("openai", {
+							env = {
+								api_key = 'cmd:echo "$OPENAI_API_KEY"',
+							},
+						})
+					end,
+					deepseek = function()
+						return require("codecompanion.adapters").extend("deepseek", {
+							env = {
+								api_key = 'cmd:echo "$DEEPSEEK_API_KEY"',
+							},
+						})
+					end,
+					anthropic = function()
+						return require("codecompanion.adapters").extend("anthropic", {
+							env = {
+								api_key = 'cmd:echo "$ANTHROPIC_API_KEY"',
+							},
+						})
+					end,
+          tavily = function()
+            return require("codecompanion.adapters").extend("tavily", {
               env = {
-                api_key = 'cmd:echo "$OPENAI_API_KEY"'
-              }
-            })
-          end,
-          deepseek = function()
-            return require("codecompanion.adapters").extend("deepseek", {
-              env = {
-                api_key = 'cmd:echo "$DEEPSEEK_API_KEY"'
+                api_key = 'cmd:echo "$TAVILY_API_KEY"',
               },
             })
           end,
-          anthropic = function()
-            return require("codecompanion.adapters").extend("anthropic", {
-              env = {
-                api_key = 'cmd:echo "$ANTHROPIC_API_KEY"'
-              },
-            })
-          end
-        }
-      })
+				},
+				extensions = {
+					history = {
+						enabled = true,
+						opts = {
+							expiration_days = 14,
+							delete_on_clearing_chat = true,
+							title_generation_opts = {
+								refresh_every_n_prompts = 1,
+                provider = "copilot",
+                model = "gpt-4o",
+							},
+							max_refreshes = 10,
+							chat_filter = function(chat_data)
+								return chat_data.cwd == vim.fn.getcwd()
+							end,
+						},
+					},
+				},
+			})
 
-      vim.keymap.set({ "n", "v" }, "<leader>cq", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
-      vim.keymap.set({ "n", "v" }, "<leader>cc", "<cmd>CodeCompanionChat Toggle<cr>",
-        { noremap = true, silent = true })
-      vim.cmd([[cab cc CodeCompanion]])
-    end,
-  },
+			vim.keymap.set(
+				{ "n", "v" },
+				"<leader>cq",
+				"<cmd>CodeCompanionActions<cr>",
+				{ noremap = true, silent = true }
+			)
+			vim.keymap.set(
+				{ "n", "v" },
+				"<leader>cc",
+				"<cmd>CodeCompanionChat Toggle<cr>",
+				{ noremap = true, silent = true }
+			)
+			vim.cmd([[cab cc CodeCompanion]])
+		end,
+	},
 }
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,90 +1,93 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local codecompanion_model = require("plugins.code-companion.lualine-model")
-    local codecompanion_adapter = require("plugins.code-companion.lualine-adapter")
-    require("lualine").setup({
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = {
-          {
-            "filename",
-            path = 1,
-          },
-        },
-        lualine_x = {
-          {
-            "lsp_status",
-            icon = "",
-          },
-          "encoding",
-          "fileformat",
-          "filetype",
-        },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-      options = {
-        section_separators = "",
-        component_separators = "",
-      },
-      extensions = {
-        {
-          filetypes = { "snacks_dashboard" },
-          sections= {}
-        },
-        {
-          filetypes = { "codecompanion" },
-          sections = {
-            lualine_a = { "mode" },
-            lualine_b = {
-              codecompanion_adapter,
-            },
-            lualine_c = {
-              codecompanion_model,
-            },
-            lualine_x = {},
-            lualine_y = {
-              "progress",
-            },
-            lualine_z = {
-              "location",
-            },
-          },
-          inactive_sections = {
-            lualine_a = {
-              codecompanion_adapter,
-              codecompanion_model,
-            },
-            lualine_b = {},
-            lualine_c = {},
-            lualine_x = {},
-            lualine_y = {},
-            lualine_z = {},
-          },
-        },
-        {
-          filetypes = { "oil" },
-          sections = {
-            lualine_a = {
-              "mode",
-            },
-            lualine_b = {
-              function()
-                local ok, oil = pcall(require, "oil")
-                if not ok then
-                  return ""
-                end
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local codecompanion_model = require("plugins.code-companion.lualine-model")
+		local codecompanion_adapter = require("plugins.code-companion.lualine-adapter")
+		local codecompanion_spinner = require("plugins.code-companion.lualine-spinner")
+		require("lualine").setup({
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+				lualine_x = {
+					{
+						"lsp_status",
+						icon = "",
+					},
+					"encoding",
+					"fileformat",
+					"filetype",
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			options = {
+				section_separators = "",
+				component_separators = "",
+			},
+			extensions = {
+				{
+					filetypes = { "snacks_dashboard" },
+					sections = {},
+				},
+				{
+					filetypes = { "codecompanion" },
+					sections = {
+						lualine_a = { "mode" },
+						lualine_b = {
+							codecompanion_adapter,
+						},
+						lualine_c = {
+							codecompanion_model,
+						},
+						lualine_x = {
+							codecompanion_spinner,
+						},
+						lualine_y = {
+							"progress",
+						},
+						lualine_z = {
+							"location",
+						},
+					},
+					inactive_sections = {
+						lualine_a = {
+							codecompanion_adapter,
+							codecompanion_model,
+						},
+						lualine_b = {},
+						lualine_c = {},
+						lualine_x = {},
+						lualine_y = {},
+						lualine_z = {},
+					},
+				},
+				{
+					filetypes = { "oil" },
+					sections = {
+						lualine_a = {
+							"mode",
+						},
+						lualine_b = {
+							function()
+								local ok, oil = pcall(require, "oil")
+								if not ok then
+									return ""
+								end
 
-                local path = vim.fn.fnamemodify(oil.get_current_dir(), ":~")
-                return path .. " %m"
-              end,
-            },
-          },
-        },
-      },
-    })
-  end,
+								local path = vim.fn.fnamemodify(oil.get_current_dir(), ":~")
+								return path .. " %m"
+							end,
+						},
+					},
+				},
+			},
+		})
+	end,
 }

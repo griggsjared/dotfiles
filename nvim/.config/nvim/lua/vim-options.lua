2f1767e091cb2dc7d713808ae4c81c494e3a3318
Jared Griggs <griggsjared@gmail.com>
2025-07-19 15:58:45 -0600

    feat(nvim): cleanup vim options file

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -1,55 +1,68 @@
-vim.cmd("set expandtab") -- Use spaces instead of tab
-vim.cmd("set tabstop=2") -- Number of spaces that a <Tab> in the file counts for
-vim.cmd("set softtabstop=2") -- Number of spaces that a <Tab> counts for while performing editing operations
-vim.cmd("set shiftwidth=2") -- Number of spaces to use for each step of (auto)indent
-vim.cmd("set smartindent") -- Do smart auto indenting when starting a new line
-vim.cmd("set autoindent") -- Copy indent from current line when starting a new line
-vim.cmd("set clipboard=unnamedplus") -- Use system clipboard
-vim.cmd("set listchars=eol:¬,tab:>·,trail:~,extends:>,precedes:<,space:␣") -- highlight special characters
+local augroup = vim.api.nvim_create_augroup("UserConfig", {})
 
-vim.g.mapleader = " "
-vim.opt.swapfile = false
-vim.opt.showmode = false
+-- General options
+vim.opt.number = true -- Line numbers
+vim.opt.relativenumber = true -- Relative line numbers
+vim.opt.cursorline = true -- Highlight current line
+vim.opt.wrap = false -- Don't wrap lines
+vim.opt.scrolloff = 10 -- Keep 10 lines above/below cursor
+vim.opt.sidescrolloff = 8 -- Keep 8 columns left/right of cursor
+vim.g.mapleader = " " -- Set leader key to space
+vim.opt.swapfile = false -- Disable swap files
+vim.opt.showmode = false -- Don't show mode in command line
+vim.opt.clipboard = "unnamedplus" -- Use system clipboard
+vim.opt.listchars = "eol:¬,tab:>·,trail:~,extends:>,precedes:<,space:␣" -- Characters to show for special characters
+vim.opt.title = true -- Enable title in terminal
+vim.opt.titlelen = 0 -- Do not shorten title
+vim.opt.titlestring = "nvim %{expand('%:p')}" -- Set title string to current file path
 
-vim.wo.number = true
-vim.wo.relativenumber = true
-vim.wo.cursorline = true
+-- Indentation options
+vim.opt.tabstop = 2 -- Tab width
+vim.opt.shiftwidth = 2 -- Indent width
+vim.opt.softtabstop = 2 -- Soft tab stop
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.smartindent = true -- Smart auto-indenting
+vim.opt.autoindent = true -- Copy indent from current line
 
-vim.opt.title = true
-vim.opt.titlelen = 0 -- do not shorten title
-vim.opt.titlestring = 'nvim %{expand("%:p")}'
+-- Set 4 space indentation for specific file types
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup,
+	pattern = { "php", "python" },
+	callback = function()
+		vim.opt_local.tabstop = 4
+		vim.opt_local.shiftwidth = 4
+	end,
+})
 
+-- Tabs Display options
+vim.opt.showtabline = 0 -- Never show tabline (0=never, 1=when multiple tabs, 2=always)
+
+--Search options
+vim.opt.ignorecase = true -- Case insensitive search
+vim.opt.smartcase = true -- Case sensitive if uppercase in search
+
+--Folding options
 vim.o.foldcolumn = "0" -- Value of 1 will show a column with the indents amounts for the line
 vim.o.foldlevel = 99 -- Using ufo provider need a large value
-vim.o.foldlevelstart = 99
-vim.o.foldenable = true
+vim.o.foldlevelstart = 99 -- Start with all folds open
+vim.o.foldenable = true -- Enable folding
 
+--Spell checking options
 vim.opt.spelllang = "en_us"
 vim.opt.spell = false -- off by default
 
---map Del and backspace to the empty register in visual mode
-vim.api.nvim_set_keymap("v", "<Del>", '"_d', { noremap = true })
-vim.api.nvim_set_keymap("v", "<BS>", '"_d', { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>sc", ":set spell!<CR>", { noremap = true }) -- Toggle for spell check
+vim.api.nvim_set_keymap("n", "<leader>sa", "zg", { noremap = true }) -- Add current word to dictionary
 
---add toggle for spell check
-vim.api.nvim_set_keymap("n", "<leader>sc", ":set spell!<CR>", { noremap = true })
--- add current word to dictionary
-vim.api.nvim_set_keymap("n", "<leader>sa", "zg", { noremap = true })
+vim.api.nvim_set_keymap("v", "<Del>", '"_d', { noremap = true }) -- Map Delete key to delete without yanking
+vim.api.nvim_set_keymap("v", "<BS>", '"_d', { noremap = true }) -- Map Backspace key to delete without yanking
 
---add close the current buffer and all buffers
-vim.api.nvim_set_keymap("n", "<leader>bc", ":bp<bar>sp<bar>bn<bar>bd<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>ba", ":bufdo bd<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>bc", ":bp<bar>sp<bar>bn<bar>bd<CR>", { noremap = true }) -- Close current buffer
+vim.api.nvim_set_keymap("n", "<leader>ba", ":bufdo bd<CR>", { noremap = true }) -- Close all buffers
+vim.api.nvim_set_keymap("n", "<leader><leader>", ":b#<CR>", { noremap = true }) -- Go to last buffer
+vim.api.nvim_set_keymap("n", "<leader>bn", ":bn<CR>", { noremap = true }) -- Go to next buffer
+vim.api.nvim_set_keymap("n", "<leader>bp", ":bp<CR>", { noremap = true }) -- Go to previous buffer
 
---go to last buffer
-vim.api.nvim_set_keymap("n", "<leader><leader>", ":b#<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>bl", ":b#<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>sh", ":set list!<CR>", { noremap = true }) --toggle to show hidden characters
 
--- go to next buffer
-vim.api.nvim_set_keymap("n", "<leader>bn", ":bn<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>bp", ":bp<CR>", { noremap = true })
-
---toggle to show hidden characters
-vim.api.nvim_set_keymap("n", "<leader>sh", ":set list!<CR>", { noremap = true })
-
---add :W to save the same as w
 vim.api.nvim_create_user_command("W", "w", { bang = false, force = true })

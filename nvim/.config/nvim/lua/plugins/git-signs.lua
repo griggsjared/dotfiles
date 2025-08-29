4306968b953712af5bc40d2d6e29e6ea2044d93b
Jared Griggs <griggsjared@gmail.com>
2025-08-29 15:57:40 -0600

    feat(nvim): made line blame git for gitsigns always active

--- nvim/.config/nvim/lua/plugins/git-signs.lua
+++ nvim/.config/nvim/lua/plugins/git-signs.lua
@@ -2,9 +2,10 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     config = function()
-      require("gitsigns").setup()
+      require("gitsigns").setup({
+        current_line_blame = true
+      })
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", { desc = "Toggle git blame for current line" })
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle git blame for current line" })
     end,
   },
 }

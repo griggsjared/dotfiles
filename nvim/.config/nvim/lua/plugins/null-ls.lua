46d6c2e13bbfee3c2fa4d688ce7f192cf76537bf
Jared Griggs <griggsjared@gmail.com>
2025-08-29 15:55:05 -0600

    feat(nvim): remove null-ls eslint and add eslint-lsp and eslint_d formatter in conform

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -1,28 +1,9 @@
 return {
 	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
 	config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
-			on_attach = function() end,
 			sources = {
-				require("none-ls.diagnostics.eslint").with({
-					prefer_local = "node_modules/.bin",
-					condition = function(utils)
-						return utils.root_has_file({
-							".eslintrc.js",
-							".eslintrc.json",
-							".eslintrc",
-							".eslintrc.yml",
-							".eslintrc.yaml",
-							"eslint.config.mjs",
-							"eslint.config.cjs",
-							"eslint.config.js",
-						})
-					end,
-				}),
 				null_ls.builtins.diagnostics.phpstan.with({
 					condition = function(utils)
 						return utils.root_has_file({

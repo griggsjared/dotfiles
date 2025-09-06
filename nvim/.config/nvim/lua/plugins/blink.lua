a6d479784afcb4d368190a64469f3354676fc256
Jared Griggs <griggsjared@gmail.com>
2025-09-05 21:16:29 -0600

    chore(nvim): change ollama blink cmp model to 1.5b param qwen

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -56,7 +56,7 @@ return {
 								api_key = "TERM",
 								name = "Ollama",
 								end_point = "http://localhost:11434/v1/completions",
-								model = "qwen2.5-coder:3b",
+                model = "qwen2.5-coder:1.5b",
 								optional = {
 									max_tokens = 256,
 									top_p = 0.9,

bb0ef4f5ecc2bc47fb1587f6ee6b531dc6ec04b2
Jared Griggs <griggsjared@gmail.com>
2025-12-14 16:22:02 -0700

    feat(nvim): better sidekick status in lualine

---
+++ nvim/.config/nvim/lua/lualine/sidekick-status.lua
@@ -0,0 +1,12 @@
+local function sidekick_status(active)
+	local cli = vim.b.sidekick_cli.name
+	if not cli then
+		return ""
+	end
+	if not active then
+		return cli
+	end
+	return "  " .. cli
+end
+
+return sidekick_status

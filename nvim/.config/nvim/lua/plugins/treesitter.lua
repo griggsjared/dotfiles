630acfcba41ed8f342b4f849312a304deb2f3598
Jared Griggs <griggsjared@gmail.com>
2025-12-30 21:16:55 -0700

    feat(nvim): config treesitter context with 5 max lines

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -27,5 +27,11 @@ return {
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-context",
+		config = function()
+			require("treesitter-context").setup({
+				enable = true,
+				max_lines = 5,
+			})
+		end,
 	},
 }

64dbfc598f4a9b5db7ea49dd8005c692dc3d4516
Jared Griggs <griggsjared@gmail.com>
2026-01-24 22:29:55 -0700

    feat(zsh): add ember theme

---
+++ nvim/.config/nvim/lua/baked/palettes/ember.lua
@@ -0,0 +1,22 @@
+local palette = {
+	background = "#1a1a1a",
+	dark1 = "#171717",
+	dark2 = "#101010",
+
+	white = "#fcfcfa",
+
+	red = "#ff6b5a",
+	orange = "#ff9b5a",
+	yellow = "#ffcf5a",
+	green = "#8fd460",
+	blue = "#5fb4d4",
+	magenta = "#d98fd4",
+
+	dimmed1 = "#c1c0c0",
+	dimmed2 = "#939293",
+	dimmed3 = "#727072",
+	dimmed4 = "#5b595c",
+	dimmed5 = "#403e41",
+}
+
+return palette

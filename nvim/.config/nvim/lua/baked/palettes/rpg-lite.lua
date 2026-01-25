47b6041c7d1fdb07d3ae0a2f20c1ce030afd467e
Jared Griggs <griggsjared@gmail.com>
2026-01-24 22:42:03 -0700

    feat(zsh): add rpg and rpg-lite themes

---
+++ nvim/.config/nvim/lua/baked/palettes/rpg-lite.lua
@@ -0,0 +1,22 @@
+local palette = {
+	background = "#1a1a1a",
+	dark1 = "#171717",
+	dark2 = "#101010",
+
+	white = "#fcfcfa",
+
+	red = "#d9726c",
+	orange = "#e6975c",
+	yellow = "#d9b85c",
+	green = "#7acc5c",
+	blue = "#4a8cbd",
+	magenta = "#9d6ac4",
+
+	dimmed1 = "#c1c0c0",
+	dimmed2 = "#939293",
+	dimmed3 = "#727072",
+	dimmed4 = "#5b595c",
+	dimmed5 = "#403e41",
+}
+
+return palette
---
+++ nvim/.config/nvim/lua/baked/palettes/rpg.lua
@@ -0,0 +1,22 @@
+local palette = {
+	background = "#1a1a1a",
+	dark1 = "#171717",
+	dark2 = "#101010",
+
+	white = "#fcfcfa",
+
+	red = "#ff4040",
+	orange = "#ff8000",
+	yellow = "#ffd100",
+	green = "#1eff00",
+	blue = "#0070dd",
+	magenta = "#a335ee",
+
+	dimmed1 = "#c1c0c0",
+	dimmed2 = "#939293",
+	dimmed3 = "#727072",
+	dimmed4 = "#5b595c",
+	dimmed5 = "#403e41",
+}
+
+return palette

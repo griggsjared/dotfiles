d7c17dea350ada7bf03505c373893d3910541cb7
Jared Griggs <griggsjared@gmail.com>
2026-02-13 13:53:43 -0700

    feat(zsh): add kport {port} function

--- zsh/.zsh/navigation.zsh
+++ zsh/.zsh/navigation.zsh
@@ -4,6 +4,24 @@
 alias ls='lsd -A'
 alias y='yazi'
 
+# Kill process listening on a port
+kport() {
+  if [[ -z "$1" ]]; then
+    echo "Usage: kport <port>"
+    return 1
+  fi
+
+  local pid
+  pid=$(lsof -ti :"$1")
+
+  if [[ -z "$pid" ]]; then
+    echo "No process is listening on port $1"
+    return 1
+  fi
+
+  kill -15 "$pid"
+}
+
 # chpwd function automatically called when directory changes
 chpwd() {
   ls

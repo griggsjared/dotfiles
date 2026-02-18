09cc668f100abe596fe3620d4e4715ff005f1296
Jared Griggs <griggsjared@gmail.com>
2026-02-17 19:44:35 -0700

    feat(zsh): reorder init.zsh better perf in autocomplete

--- zsh/.zsh/init.zsh
+++ zsh/.zsh/init.zsh
@@ -3,22 +3,32 @@
 # Set XDG config home
 export XDG_CONFIG_HOME="$HOME/.config"
 
-# Load Docker init if available
-[[ -f "$HOME/.docker/init-zsh.sh" ]] && source "$HOME/.docker/init-zsh.sh"
-
-# ZSH plugins
-source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh || true
-source /opt/homebrew/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh || true
-source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh || true
-
 # ZSH autocomplete functions
 if type brew &>/dev/null; then
     FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
 
     setopt completealiases
     autoload -Uz compinit
-    compinit -u
+    
+    compinit -i
 fi
+
+# Load Docker init if available
+[[ -f "$HOME/.docker/init-zsh.sh" ]] && source "$HOME/.docker/init-zsh.sh"
 
 # NVM bash completion
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+
+# ZSH plugins loading
+
+# Autosuggestions
+source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh || true
+
+# Autocomplete
+zstyle ':autocomplete:*' min-input 2
+zstyle ':autocomplete:*' delay 0.1
+zstyle ':autocomplete:*' max-lines 10
+source /opt/homebrew/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh || true
+
+# Syntax highlighting
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh || true

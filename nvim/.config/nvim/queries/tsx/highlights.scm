ee1636e042e12684be5ac423c0ece2d2392b230c
Jared Griggs <griggsjared@gmail.com>
2026-04-05 09:45:24 -0600

    feat(nvim)!: plugins and tree sitter updates for nvim 0.12

---
+++ nvim/.config/nvim/queries/tsx/highlights.scm
@@ -0,0 +1,6 @@
+; extends
+
+(jsx_opening_element name: (identifier) @tag.builtin)
+(jsx_closing_element name: (identifier) @tag.builtin)
+(jsx_self_closing_element name: (identifier) @tag.builtin)
+(jsx_attribute (property_identifier) @tag.attribute)

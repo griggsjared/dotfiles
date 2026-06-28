65b44375fbda3ea156495c4438b05f0e568b04dc
Jared Griggs <griggsjared@gmail.com>
2026-06-28 13:59:28 -0600

    feat(nvim): invert lualine A group background with foreground

--- nvim/.config/nvim/lua/baked/theme/plugins/lualine.lua
+++ nvim/.config/nvim/lua/baked/theme/plugins/lualine.lua
@@ -2,13 +2,13 @@ local M = {}
 
 function M.get(c, _)
 	return {
-		LualineNormalA = { bg = c.base.green, fg = c.base.dark, bold = true },
+		LualineNormalA = { bg = "NONE", fg = c.base.green, bold = true },
 		LualineNormalB = { bg = "NONE", fg = c.base.white },
 		LualineNormalC = { bg = "NONE", fg = c.base.white },
-		LualineInsertA = { bg = c.base.blue, fg = c.base.dark, bold = true },
-		LualineVisualA = { bg = c.base.magenta, fg = c.base.dark, bold = true },
-		LualineReplaceA = { bg = c.base.red, fg = c.base.dark, bold = true },
-		LualineCommandA = { bg = c.base.yellow, fg = c.base.dark, bold = true },
+		LualineInsertA = { bg = "NONE", fg = c.base.blue, bold = true },
+		LualineVisualA = { bg = "NONE", fg = c.base.magenta, bold = true },
+		LualineReplaceA = { bg = "NONE", fg = c.base.red, bold = true },
+		LualineCommandA = { bg = "NONE", fg = c.base.yellow, bold = true },
 		LualineInactiveA = { bg = "NONE", fg = c.base.dimmed1 },
 		LualineInactiveB = { bg = "NONE", fg = c.base.dimmed1 },
 		LualineInactiveC = { bg = "NONE", fg = c.base.dimmed1 },

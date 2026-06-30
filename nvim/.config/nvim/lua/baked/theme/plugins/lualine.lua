b0a8f67483820e678ec224e9ad0a318f6ec6dcec
Jared Griggs <griggsjared@gmail.com>
2026-06-29 18:30:27 -0600

    feat(nvim): remove some bold from lualine items

--- nvim/.config/nvim/lua/baked/theme/plugins/lualine.lua
+++ nvim/.config/nvim/lua/baked/theme/plugins/lualine.lua
@@ -2,13 +2,13 @@ local M = {}
 
 function M.get(c, _)
 	return {
-		LualineNormalA = { bg = "NONE", fg = c.base.green, bold = true },
+		LualineNormalA = { bg = "NONE", fg = c.base.green },
 		LualineNormalB = { bg = "NONE", fg = c.base.white },
 		LualineNormalC = { bg = "NONE", fg = c.base.white },
-		LualineInsertA = { bg = "NONE", fg = c.base.blue, bold = true },
-		LualineVisualA = { bg = "NONE", fg = c.base.magenta, bold = true },
-		LualineReplaceA = { bg = "NONE", fg = c.base.red, bold = true },
-		LualineCommandA = { bg = "NONE", fg = c.base.yellow, bold = true },
+		LualineInsertA = { bg = "NONE", fg = c.base.blue },
+		LualineVisualA = { bg = "NONE", fg = c.base.magenta },
+		LualineReplaceA = { bg = "NONE", fg = c.base.red },
+		LualineCommandA = { bg = "NONE", fg = c.base.yellow },
 		LualineInactiveA = { bg = "NONE", fg = c.base.dimmed1 },
 		LualineInactiveB = { bg = "NONE", fg = c.base.dimmed1 },
 		LualineInactiveC = { bg = "NONE", fg = c.base.dimmed1 },

e310aa7d1fd56bae0e74d30109088436a2105d54
Jared Griggs <griggsjared@gmail.com>
2026-07-03 19:23:26 -0600

    chore(nvim): add condtional loading for rpg theme local first

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -1,6 +1,10 @@
+local rpg_path = vim.fn.expand("~/projects/rpg-nvim")
+local rpg_spec = vim.fn.isdirectory(rpg_path) == 1
+	and { dir = rpg_path }
+	or { "griggsjared/rpg.nvim" }
+
 return {
-	{
-		dir = vim.fn.stdpath("config") .. "/rpg.nvim",
+	vim.tbl_extend("force", rpg_spec, {
 		priority = 1000,
 		lazy = false,
 		config = function()
@@ -9,5 +13,5 @@ return {
 			})
 			vim.cmd([[colorscheme rpg]])
 		end,
-	},
+	}),
 }

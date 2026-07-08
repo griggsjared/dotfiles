4d63f7d89f00393f8ff6301b176bc72df8999bf5
Jared Griggs <griggsjared@gmail.com>
2026-07-08 16:27:01 -0600

    feat(nvim): add gitsigns blame ui toggle keybind

--- nvim/.config/nvim/lua/plugins/git-signs.lua
+++ nvim/.config/nvim/lua/plugins/git-signs.lua
@@ -2,12 +2,28 @@ return {
 	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
-			require("gitsigns").setup({
+			local gitsigns = require("gitsigns")
+			gitsigns.setup({
 				current_line_blame = true,
 			})
+
+			local function toggle_blame()
+				for _, win in ipairs(vim.api.nvim_tabpage_list_wins(0)) do
+					local buf = vim.api.nvim_win_get_buf(win)
+					if vim.bo[buf].filetype == "gitsigns-blame" then
+						vim.api.nvim_win_close(win, true)
+						return
+					end
+				end
+
+				gitsigns.blame()
+			end
+
 			vim.keymap.set("n", "<leader>pp", ":Gitsigns preview_hunk_inline<CR>", { desc = "Preview hunk" })
 			vim.keymap.set("n", "<leader>pr", ":Gitsigns reset_hunk<CR>", { desc = "Reset git hunk" })
 			vim.keymap.set("n", "<leader>ps", ":Gitsigns stage_hunk<CR>", { desc = "Stage git hunk" })
+			vim.keymap.set("n", "<leader>pb", toggle_blame, { desc = "Toggle full git blame UI" })
+			vim.keymap.set("n", "<leader>pl", ":Gitsigns toggle_current_line_blame<CR>", { desc = "Toggle current line blame" })
 		end,
 	},
 }

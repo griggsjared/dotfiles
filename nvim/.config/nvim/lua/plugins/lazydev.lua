c6badb77bcd0ba4ce443d1d5e89604a6e2b18ecc
Jared Griggs <griggsjared@gmail.com>
2026-07-19 15:35:02 -0600

    feat(nvim): add lazydev

---
+++ nvim/.config/nvim/lua/plugins/lazydev.lua
@@ -0,0 +1,13 @@
+---@type LazySpec
+return {
+	"folke/lazydev.nvim",
+	ft = "lua",
+	---@module "lazydev"
+	---@type lazydev.Config
+	opts = {
+		library = {
+			{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			{ path = "snacks.nvim", words = { "Snacks" } },
+		},
+	},
+}

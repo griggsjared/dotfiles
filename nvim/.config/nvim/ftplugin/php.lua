afafad06d72086eb19288ac47ab214318111b044
Jared Griggs <griggsjared@gmail.com>
2026-07-19 15:41:18 -0600

    fix(nvim): remove dead php indentexpr

--- nvim/.config/nvim/ftplugin/php.lua
+++ nvim/.config/nvim/ftplugin/php.lua
@@ -1,6 +1,5 @@
 require("nvim-treesitter").install({ "php", "php_only" })
 vim.treesitter.start()
-vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4

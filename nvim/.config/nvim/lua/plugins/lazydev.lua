84408836e63329930f0e89e920a7f30bd52554b4
Jared Griggs <griggsjared@gmail.com>
2026-07-19 15:53:38 -0600

    fix(nvim): add lazy.nvim types to lazydev library

--- nvim/.config/nvim/lua/plugins/lazydev.lua
+++ nvim/.config/nvim/lua/plugins/lazydev.lua
@@ -7,6 +7,7 @@ return {
 	opts = {
 		library = {
 			{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			{ path = "lazy.nvim", words = { "LazySpec", "LazyKeysSpec", "LazyPluginSpec" } },
 			{ path = "snacks.nvim", words = { "Snacks" } },
 		},
 	},

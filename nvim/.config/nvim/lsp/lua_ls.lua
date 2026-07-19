04f5ea142238ff9f7eb5fcf8e749d44995cb6c96
Jared Griggs <griggsjared@gmail.com>
2026-07-19 15:35:54 -0600

    feat(nvim): update .luarc.json

--- nvim/.config/nvim/lsp/lua_ls.lua
+++ nvim/.config/nvim/lsp/lua_ls.lua
@@ -1,12 +1,9 @@
+---@type vim.lsp.Config
 return {
 	settings = {
 		Lua = {
 			diagnostics = {
 				globals = { "vim" },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
 			},
 			telemetry = {
 				enable = false,

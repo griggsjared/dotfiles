90e00e561c404d6c85f262d1cae928d0d664add7
Jared Griggs <griggsjared@gmail.com>
2026-07-19 20:38:04 -0600

    fix(nvim): clean up oil spec shape and missing annotations

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,57 +1,58 @@
+---@type LazySpec
 return {
-	{
-		"stevearc/oil.nvim",
-		dependencies = { { "echasnovski/mini.icons", opts = {} } },
-		config = function()
-			function _G.get_oil_winbar()
-				local dir = require("oil").get_current_dir()
-				if dir then
-					return vim.fn.fnamemodify(dir, ":~")
-				else
-					return vim.api.nvim_buf_get_name(0)
-				end
+	"stevearc/oil.nvim",
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	config = function()
+		-- must stay as _G for the v:lua call in win_options.winbar
+		---@return string
+		function _G.get_oil_winbar()
+			local dir = require("oil").get_current_dir()
+			if dir then
+				return vim.fn.fnamemodify(dir, ":~")
+			else
+				return vim.api.nvim_buf_get_name(0)
 			end
+		end
 
-			local detail = false
-			local hidden_prefixes = { ".git/", ".DS_Store", ".null-ls" }
+		local detail = false
+		local hidden_prefixes = { ".git/", ".DS_Store", ".null-ls" }
 
-			require("oil").setup({
-				default_file_explorer = true,
-				delete_to_trash = true,
-				skip_confirm_for_simple_edits = true,
-				win_options = {
-					winbar = "%!v:lua.get_oil_winbar()",
-				},
-				view_options = {
-					show_hidden = true,
-					is_always_hidden = function(name)
-						for _, prefix in ipairs(hidden_prefixes) do
-							if vim.startswith(name, prefix) then
-								return true
-							end
+		require("oil").setup({
+			default_file_explorer = true,
+			delete_to_trash = true,
+			skip_confirm_for_simple_edits = true,
+			win_options = {
+				winbar = "%!v:lua.get_oil_winbar()",
+			},
+			view_options = {
+				show_hidden = true,
+				is_always_hidden = function(name)
+					for _, prefix in ipairs(hidden_prefixes) do
+						if vim.startswith(name, prefix) then
+							return true
+						end
+					end
+				end,
+			},
+			keymaps = {
+				["<C-p>"] = { "actions.preview", opts = { vertical = true, split = "botright" } },
+				["<leader><esc>"] = "actions.close",
+				["q"] = "actions.close",
+				["gd"] = {
+					desc = "Toggle file detail view",
+					callback = function()
+						detail = not detail
+						if detail then
+							require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+						else
+							require("oil").set_columns({ "icon" })
 						end
 					end,
 				},
-				keymaps = {
-					["<C-p>"] = { "actions.preview", opts = { vertical = true, split = "botright" } },
-					["<leader><esc>"] = "actions.close",
-					["q"] = "actions.close",
-					["gd"] = {
-						desc = "Toggle file detail view",
-						callback = function()
-							detail = not detail
-							if detail then
-								require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
-							else
-								require("oil").set_columns({ "icon" })
-							end
-						end,
-					},
-				},
-			})
-			vim.keymap.set("n", "-", function()
-				require("oil").open()
-			end, { desc = "Open parent directory (oil.nvim)" })
-		end,
-	},
+			},
+		})
+		vim.keymap.set("n", "-", function()
+			require("oil").open()
+		end, { desc = "Open parent directory (oil.nvim)" })
+	end,
 }

4771991fce16d7c9ba04d3bd988417dcf0cebe7c
Jared Griggs <griggsjared@gmail.com>
2026-07-19 18:28:14 -0600

    refactor(nvim): dedupe cssls settings, tsx queries inherits js

--- nvim/.config/nvim/lsp/cssls.lua
+++ nvim/.config/nvim/lsp/cssls.lua
@@ -1,22 +1,13 @@
+local lang_settings = {
+	validate = true,
+	lint = { unknownAtRules = "ignore" },
+}
+
+---@type vim.lsp.Config
 return {
 	settings = {
-		css = {
-			validate = true,
-			lint = {
-				unknownAtRules = "ignore",
-			},
-		},
-		less = {
-			validate = true,
-			lint = {
-				unknownAtRules = "ignore",
-			},
-		},
-		scss = {
-			validate = true,
-			lint = {
-				unknownAtRules = "ignore",
-			},
-		},
+		css = lang_settings,
+		less = lang_settings,
+		scss = lang_settings,
 	},
 }

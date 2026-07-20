027a6d10e061c1b942d857658d11b9b06fc14bc2
Jared Griggs <griggsjared@gmail.com>
2026-07-20 08:15:10 -0600

    fix(nvim): fix broken react tsx hl queries

--- nvim/.config/nvim/queries/tsx/highlights.scm
+++ nvim/.config/nvim/queries/tsx/highlights.scm
@@ -1,1 +1,6 @@
-; inherits: javascript
+; extends
+
+(jsx_opening_element name: (identifier) @tag.builtin)
+(jsx_closing_element name: (identifier) @tag.builtin)
+(jsx_self_closing_element name: (identifier) @tag.builtin)
+(jsx_attribute (property_identifier) @tag.attribute)
